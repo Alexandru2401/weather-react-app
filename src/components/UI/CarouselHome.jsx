3ab136996b2325e2c49828a205cba87570c1f68a
Alexandru2401 <a.toma231@yahoo.com>
2024-09-16 11:32:18 +0300

    make carousel working

--- src/components/UI/CarouselHome.jsx
+++ src/components/UI/CarouselHome.jsx
@@ -4,6 +4,16 @@ import { items } from "../../api/carouselContent";
 import { useState } from "react";
 export default function CarouselHome() {
   const [index, setIndex] = useState(0);
+
+  function handleNext() {
+    setIndex((prevIndex) => (prevIndex + 1) % items.length);
+  }
+
+  function handlePrev() {
+    setIndex((prevIndex) => (prevIndex - 1 + items.length) % items.length);
+  }
+
+  let item = items[index];
   return (
     <>
       <h2 className="text-center m-3">Title</h2>
@@ -12,9 +22,11 @@ export default function CarouselHome() {
           <img src={carouselImg} />
         </div>
         <div className="w-2/4">
-          {items.map((item) => (
-            <Card>{item.content}</Card>
-          ))}
+          <button onClick={handlePrev}>Prev</button>
+
+          <Card>{item.content}</Card>
+
+          <button onClick={handleNext}>Next</button>
         </div>
       </div>
     </>
