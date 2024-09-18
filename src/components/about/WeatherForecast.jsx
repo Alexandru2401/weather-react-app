54438f1aee9502323850f27db8b3e5826aff957f
Alexandru2401 <a.toma231@yahoo.com>
2024-09-18 18:44:46 +0300

    make the skeleton for weather forecast component

---
+++ src/components/about/WeatherForecast.jsx
@@ -0,0 +1,51 @@
+import PlaceIcon from "@mui/icons-material/Place";
+import TodayIcon from "@mui/icons-material/Today";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import ThermostatIcon from "@mui/icons-material/Thermostat";
+import AirIcon from "@mui/icons-material/Air";
+import WaterDropIcon from "@mui/icons-material/WaterDrop";
+import DescriptionIcon from "@mui/icons-material/Description";
+export default function WeatherForecast() {
+  const basicStyle =
+    "text-2xl text-slate-900 mb-3 shadow-2xl p-3 rounded-2xl backdrop-blur-2xl";
+  const bg =
+    " linear-gradient(22deg, rgba(142,215,247,1) 1%, rgba(219,219,231,1) 50%, rgba(156,188,235,1) 100%)";
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="mt-10 mb-5 text-center text-5xl font-semibold  text-transparent bg-gradient-to-r from-slate-300 via-gray-700 to-black bg-clip-text py-3">
+        Current City:
+      </h2>
+      <div
+        style={{ background: bg }}
+        className="w-3/4 my-4 p-3 flex items-center justify-around shadow-md shadow-slate-600 rounded-2xl"
+      >
+        <div>
+          <h3 className="text-4xl text-slate-900 my-4 shadow-2xl p-3 rounded-2xl backdrop-blur-lg">
+            <PlaceIcon /> City:
+          </h3>
+          <p className={basicStyle}>
+            <TodayIcon /> Date:
+          </p>
+          <p className={basicStyle}>
+            <AccessTimeIcon />
+          </p>
+          <p className={basicStyle}>
+            <ThermostatIcon />
+          </p>
+        </div>
+        <div>
+          <p className={basicStyle}>Real feel:&deg; C</p>
+          <p className={basicStyle}>
+            <DescriptionIcon />
+            Description:
+          </p>
+          {/* <img src={iconUrl} alt="" /> */}
+          <p className={basicStyle}>
+            <AirIcon />
+            Wind: km/h
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,13 @@
+import WeatherForecast from "../components/about/WeatherForecast";
+
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div>
+      <section>
+        <WeatherForecast />
+      </section>
+    </div>
+  );
 }
 // 1. A short description about the application
 // 2. Here will be displayed weather forecast fot 10 days for a selected city (by default Bucharest)
