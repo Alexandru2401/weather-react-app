f35d18539964f2deea4105f307c28c5ceacc1b04
Alexandru2401 <a.toma231@yahoo.com>
2024-09-20 17:21:06 +0300

    working on mobileApp component to make it responsive

--- src/components/home/MobileApp.jsx
+++ src/components/home/MobileApp.jsx
@@ -10,30 +10,30 @@ import { Link } from "react-router-dom";
 
 export default function MobileApp() {
   const spanStyle =
-    "shadow-2xl p-3 rounded-2xl backdrop-blur-lg absolute text-xl text-slate-700 shadow-md";
+    "shadow-2xl p-1 lg:p-3 text-md rounded-2xl backdrop-blur-lg absolute lg:text-xl text-slate-700 shadow-md";
   const shadow = "shadow-md shadow-slate-700";
   return (
     <section className="flex flex-col justify-center items-center relative">
-      <h2 className="text-5xl font-semibold my-10 text-slate-950">
+      <h2 className="text-2xl lg:text-5xl font-semibold my-10 text-slate-950">
         Weather forecast in your pocket!
       </h2>
-      <div className="w-2/4 min-h-[700px] flex items-center justify-center rounded-full shadow-sm border-dashed border-black border-2 my-9">
-        <div className={`${spanStyle} ${shadow} right-20 top-1/4`}>
+      <div className="w-full lg:w-2/4 min-h-[500px] lg:min-h-[700px] flex items-center justify-center rounded-full shadow-sm border-dashed border-black border-2 mb-10 md:mb-20 lg:my-9 relative">
+        <div className={`${spanStyle} ${shadow} -top-5`}>
           <h3>Get Weather Alerts Fast</h3>
           <p>Be Prepared for Anything with Instant Notifications!</p>
         </div>
-        <div className={`${spanStyle} ${shadow} left-14 `}>
+        <div className={`${spanStyle} ${shadow} lg:-left-72`}>
           <h3>Plan Your Day with Confidence</h3>
           <p>Get Precise Weather Updates Anytime, Anywhere!</p>
         </div>
-        <div className={`${spanStyle} ${shadow} bottom-3`}>
+        <div className={`${spanStyle} ${shadow} -bottom-6`}>
           <h3>Rain or Shine, We've Got You Covered</h3>{" "}
           <p> The Ultimate Weather Companion!</p>
         </div>
-        <span className={`${spanStyle} ${shadow} right-32`}>
+        <span className={`${spanStyle} ${shadow} top-1/4 -left-2 lg:-left-20`}>
           Temperature in your location
         </span>
-        <span className={`${spanStyle} ${shadow} left-80 top-1/3`}>
+        <span className={`${spanStyle} ${shadow} top-2/3 right-5 md:-right-12`}>
           Daily forecast
         </span>
         <img
@@ -42,7 +42,7 @@ export default function MobileApp() {
           alt="img-phone"
         />
       </div>
-      <div className="absolute bottom-4 left-2 p-3 rounded-3xl bg-slate-400 flex items-center justify-center">
+      <div className="md:absolute md:-bottom-8 bottom-4 left-2 p-3 rounded-3xl bg-slate-400 flex items-center justify-center">
         <Link className="mx-2 bg-slate-100 rounded-full p-1">
           <DownloadIcon />
         </Link>
@@ -56,7 +56,7 @@ export default function MobileApp() {
           App in your pocket! Download it!
         </span>
       </div>
-      <div className="absolute top-12 right-3 flex flex-col gap-2">
+      <div className="mt-10 sm:absolute sm:bottom-0 sm:right-0 lg:absolute lg:top-12 lg:right-3 flex lg:flex-col gap-2">
         {" "}
         <span className="bg-slate-100 rounded-full p-1">
           <ShareIcon />
