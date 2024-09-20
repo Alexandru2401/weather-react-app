7e81f28201f4184790b95ee818d5e6a009b64bca
Alexandru2401 <a.toma231@yahoo.com>
2024-09-20 21:17:35 +0300

    make navbar responsive and working on header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,67 +8,70 @@ import ShareIcon from "@mui/icons-material/Share";
 
 export default function Header() {
   const basicCardStyle =
-    "flex flex-col bg-transparent p-3 m-2 text-xl text-slate-100 rounded-md shadow-md shadow-slate-600 backdrop-blur-md";
+    "w-[350px] flex flex-col bg-transparent p-3 m-2 text-xl text-slate-100 rounded-md shadow-md shadow-slate-600 backdrop-blur-md";
 
   return (
-    <header className="relative h-auto sm:h-[50vh] md:h-[80vh] flex justify-around w-full bg-bgImg bg-no-repeat bg-top bg-cover rounded-b-3xl">
-      <section>
-        <h1 className="my-16 font-semibold  text-transparent bg-gradient-to-r from-slate-300 via-gray-700 to-black bg-clip-text py-3 md:text-4xl lg:text-5xl xl:text-7xl md:my-10">
+    <header className="relative min-h-[80vh] flex flex-col w-full justify-around bg-bgImg bg-no-repeat bg-top bg-cover rounded-b-3xl">
+      <div className="flex mx-auto">
+        <h1 className="m-2 lg:my-4 font-semibold  text-transparent bg-gradient-to-r from-slate-300 via-gray-700 to-black bg-clip-text py-3 text-4xl lg:text-5xl xl:text-7xl md:my-10">
           See wheather in your city!
         </h1>
-        <div className="w-60 my-12">
-          <p className="text-slate-950 md:text-lg xl:text-xl shadow-2xl shadow-slate-600 p-3 rounded-2xl backdrop-blur-md">
-            Whether it's a sunny day or an incoming storm, our detailed
-            predictions help you plan your day with confidence.
-          </p>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+      </div>
+      <section className="flex flex-col">
+        <div className="my-5 items-center gap-3 lg:my-2 flex flex-col lg:flex-row lg:justify-evenly">
+          <div className="w-3/4 lg:w-1/3">
+            <p className="text-slate-950 md:text-lg xl:text-xl shadow-2xl shadow-slate-600 p-3 rounded-2xl backdrop-blur-md">
+              Whether it's a sunny day or an incoming storm, our detailed
+              predictions help you plan your day with confidence.
+            </p>
+          </div>
+          <div>
             <Button className="bg-slate-400 w-44 shadow-md shadow-slate-800">
               <Link to="/about">
                 Get started! <ArrowForwardIcon />
               </Link>
             </Button>
           </div>
-          <div className="w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 my-16">
-            <div className={basicCardStyle}>
-              <p>
-                20&deg;<span>C</span>
-              </p>
-              <p>Weather type</p>
-              <p>City</p>
-            </div>
-            <div className={basicCardStyle}>
-              <p>
-                20&deg;<span>C</span>
-              </p>
-              <p>Weather type</p>
-              <p>City</p>
-            </div>
-            <div className={basicCardStyle}>
-              <p>
-                20&deg;<span>C</span>
-              </p>
-              <p>Weather type</p>
-              <p>City</p>
-            </div>
+          <div className="w-3/4 lg:w-1/3 backdrop-blur-md shadow-md rounded-xl p-3">
+            <p className="sm:mb-1 md:mb-5 md:text-4xl lg:text-8xl text-slate-100">
+              40&deg; C
+            </p>
+            <p className="sm:mb-1 md:mb-5 md:text-lg xl:text-2xl text-slate-100">
+              Real feel: 40&deg; C
+            </p>
+            <p className="sm:mb-1 md:mb-5 md:text-lg xl:text-2xl text-slate-100">
+              Weather today: sunny
+            </p>
+            <p className="sm:mb-1 md:mb-5 md:text-lg xl:text-2xl text-slate-100">
+              Wind: <span>20km/h</span>
+            </p>
           </div>
         </div>
+        <section className="flex items-center flex-col lg:flex-row lg:justify-evenly">
+          <div className={basicCardStyle}>
+            <p>
+              20&deg;<span>C</span>
+            </p>
+            <p>Weather type</p>
+            <p>City</p>
+          </div>
+          <div className={basicCardStyle}>
+            <p>
+              20&deg;<span>C</span>
+            </p>
+            <p>Weather type</p>
+            <p>City</p>
+          </div>
+          <div className={basicCardStyle}>
+            <p>
+              20&deg;<span>C</span>
+            </p>
+            <p>Weather type</p>
+            <p>City</p>
+          </div>
+        </section>
       </section>
-      <section className="min-h-11 sm:min-h-24 sm:p-1 md:h-52 md:p-3 lg:h-72 lg:p-4 sm:my-2 md:my-36 lg:my-44 lg:mx-10 backdrop-blur-md shadow-md rounded-xl">
-        <div>
-          <p className="sm:mb-1 md:mb-5 md:text-4xl lg:text-8xl text-slate-100">40&deg; C</p>
-          <p className="sm:mb-1 md:mb-5 md:text-lg xl:text-2xl text-slate-100">
-            Real feel: 40&deg; C
-          </p>
-          <p className="sm:mb-1 md:mb-5 md:text-lg xl:text-2xl text-slate-100">
-            Weather today: sunny
-          </p>
-          <p className="sm:mb-1 md:mb-5 md:text-lg xl:text-2xl text-slate-100">
-            Wind: <span>20km/h</span>
-          </p>
-        </div>
-      </section>
-
-      <div className="flex flex-col gap-2 sm:gap-1 my-3 fixed top-1/3 right-3">
+      <div className="flex flex-col gap-2 sm:gap-1 my-3 top-1/3 right-3 absolute">
         <span className="bg-slate-100 rounded-full p-2">
           <ShareIcon />
         </span>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,113 @@ import {
   formattedMinutes,
   day,
   currentMonths,
-  monthDay
+  monthDay,
 } from "../utils/currentDate";
 import Button from "./UI/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth <= 800) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+        setShowMenu(false); // Ascunde meniul când ecranul devine mai mare
+      }
+    }
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function handleShowMenu() {
+    setShowMenu(true);
+  }
+
+  function handleCloseMenu() {
+    setShowMenu(false);
+  }
+
   return (
-    <nav className="flex items-center justify-around bg-slate-400 text-slate-200 p-4 sticky top-0 z-10 font-semibold text-lg">
-      <div>Weather Forecast</div>
-      <ul className="flex items-center justify-center">
-        <li className="p-1 m-1">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="p-1 m-1">
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <div>
-        {formattedHours}:{formattedMinutes}
-      </div>
-      <Button className="bg-slate-700">Dark mode</Button>
-      <div>
-        {day}, {monthDay} {currentMonths}
-      </div>
-      <ul>
-        <Link className="m-2">
-          <Button className="bg-slate-900">Sign in</Button>
-        </Link>
-        <Link>
-          <Button className="bg-slate-300">Sign in</Button>
-        </Link>
-      </ul>
-    </nav> 
+    <nav className="bg-slate-400 text-slate-200 p-4 sticky top-0 z-10 font-semibold text-lg">
+      {isVisible ? (
+        <>
+          <div className="flex items-center justify-around">
+            <div>Weather Forecast</div>
+            <ul className="flex items-center justify-center">
+              <li className="p-1 m-1">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="p-1 m-1">
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+            <div>
+              {formattedHours}:{formattedMinutes}
+            </div>
+            <Button className="bg-slate-700">Dark mode</Button>
+            <div>
+              {day}, {monthDay} {currentMonths}
+            </div>
+            <ul>
+              <Link to="/signin" className="m-2">
+                <Button className="bg-slate-900">Sign in</Button>
+              </Link>
+              <Link to="/signin">
+                <Button className="bg-slate-300">Sign in</Button>
+              </Link>
+            </ul>
+          </div>
+        </>
+      ) : (
+        <div>
+          <button onClick={handleShowMenu}>
+            <MenuIcon />
+          </button>
+          <div
+            className={`fixed top-0 left-0 h-full w-64 bg-slate-400 p-4 transform transition-transform duration-300 ${
+              showMenu ? "translate-x-0" : "-translate-x-full"
+            }`}
+          >
+            <button onClick={handleCloseMenu} className="mb-4">
+              <CloseIcon />
+            </button>
+            <ul className="flex flex-col space-y-4">
+              <li>
+                <Link to="/" onClick={handleCloseMenu}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={handleCloseMenu}>
+                  About
+                </Link>
+              </li>
+              <li>
+                <Button className="bg-slate-700">Dark mode</Button>
+              </li>
+              <li>
+                <Link to="/signin">
+                  <Button className="bg-slate-900">Sign in</Button>
+                </Link>
+              </li>
+              <li>
+                <Link to="/signin">
+                  <Button className="bg-slate-300">Sign in</Button>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </nav>
   );
 }
